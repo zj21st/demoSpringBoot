ec354bd6e2b0302cc21119f3b6feb1d6623c5ae5
K41 <zhoujiana@yonyou.com>
2017-11-28 17:07:26 +0800

    注册到eureka-server-ha中的http://peer1:1111/eureka/,http://peer2:1112/eureka/

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,6 +6,8 @@ package com.example.demo.controller;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +23,16 @@ public class HelloController {
 	
 	private  final Logger  logger = Logger.getLogger(getClass());
 	
-/*	@Autowired
-	private DiscoveryClient clinet;*/
+	@Autowired
+	private DiscoveryClient clinet;
 	
 	@Value("${book.name}")
 	private String bookname;
 
 	@RequestMapping("/hello")
 	public String index() {
-		//ServiceInstance instance = clinet.getLocalServiceInstance();
-		//logger.info("/host host:"+instance.getHost()+",service_id: "+instance.getServiceId());
+		ServiceInstance instance = clinet.getLocalServiceInstance();
+		logger.info("/host host:"+instance.getHost()+",service_id: "+instance.getServiceId());
 		return "Hello World  "+bookname;
 	}
 	
