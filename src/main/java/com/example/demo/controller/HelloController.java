603c1e82d3f45962cb55b7d26a8a0e5677a11cd7
K41 <zhoujiana@yonyou.com>
2017-11-28 16:42:22 +0800

    build添加maven-plugin

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,8 @@
  */
 package com.example.demo.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	
+	private  final Logger  logger = Logger.getLogger(getClass());
+	
+/*	@Autowired
+	private DiscoveryClient clinet;*/
+	
 	@Value("${book.name}")
 	private String bookname;
 
 	@RequestMapping("/hello")
 	public String index() {
+		//ServiceInstance instance = clinet.getLocalServiceInstance();
+		//logger.info("/host host:"+instance.getHost()+",service_id: "+instance.getServiceId());
 		return "Hello World  "+bookname;
 	}
 	
